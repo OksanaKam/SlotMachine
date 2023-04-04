2daa665b2f4703e064c6cba1eea2a4a48ce970ec
OksanaKam <moremore1@yandex.ru>
2023-04-04 16:04:06 +0200

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Оксана Каменчук on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
